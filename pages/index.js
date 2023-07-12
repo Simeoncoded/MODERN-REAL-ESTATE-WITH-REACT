7a9fb1a036464983f579586f3713ebbfdf6810b2
Simeoncoded <simeonolawore07@gmail.com>
2023-07-12 03:02:17 -1100

    third commit

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Image from 'next/image';
 import { Flex,Box, Text, Button } from '@chakra-ui/react';
 import { baseUrl, fethApi } from '../utils/fetchApi';
 
+import Property from '../components/Property';
+
 const Banner = ({purpose,title1,title2,desc1,desc2,buttonText,linkName,imageUrl}) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
     <Image src={imageUrl} width={500} height={300} alt="banner"/>
@@ -45,7 +47,10 @@ export default function Home({ propertiesForSale, propertiesForRent}) {
       imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
 
     />
-      {propertiesForSale.map((property) => <Property property={property} key={property.id} />)} 
+    
+      <Flex flexWrap="wrap">
+        {propertiesForSale.map((property) => <Property property={property} key={property.id} />)} 
+      </Flex>
     </Box>
   )
 }
