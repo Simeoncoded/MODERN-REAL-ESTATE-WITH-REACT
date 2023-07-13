5af6f15d14953bbe0e75c7b8ac12b4d48de11015
Simeoncoded <simeonolawore07@gmail.com>
2023-07-13 00:36:09 -1100

    fourth commit

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const baseUrl = 'https://bayut.p.rapidapi.com'
+export const baseUrl = 'https://bayut.p.rapidapi.com';
 
 
- export const fethApi = async (url) => {
+ export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers: {
             'X-RapidAPI-Key': '0185fb8c1dmsh93b745225e8e094p1f7a64jsne9e40aa8ff61',
@@ -14,3 +14,4 @@ export const baseUrl = 'https://bayut.p.rapidapi.com'
 
     return data;
  }
+
